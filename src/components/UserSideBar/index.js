c24400b691ac304f233ad6ab5f45805cef92fb5a
Guilherme Ciole <guilhermeciole@gmail.com>
2021-08-12 18:36:41 -0300

    fix: Auteração na posição de nome e mail do usuário. Criação da pasta do componente em components.

--- src/components/UserSideBar/index.js
+++ src/components/UserSideBar/index.js
@@ -1,4 +1,4 @@
-import { Container } from "./style";
+import { Container, ContentName, ContentEmail } from "./style";
 import { FaUserCircle } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { RiPencilFill } from "react-icons/ri";
@@ -6,23 +6,24 @@ import { RiPencilFill } from "react-icons/ri";
 const UserSideBar = () => {
   return (
     <Container>
-      <image>
+      <div>
         <img
           src="https://w7.pngwing.com/pngs/181/273/png-transparent-playerunknown-s-battlegrounds-computer-icons-user-avatar-miscellaneous-video-game-fictional-character.png"
-          width="150"
+          width="200"
           alt="avatar de usuário"
         ></img>
         <RiPencilFill color="#FF6109" size="30" />
-      </image>
-      <contentName>
-        <FaUserCircle color="#FF6109" size="15" />
-        <h2>Guilherme Ciole</h2>
-      </contentName>
-      <contentEmail>
-        <MdEmail color="#FF6109" size="15" />
-        <p>guilhermeciole@email.com</p>
-      </contentEmail>
-      <iconPencil></iconPencil>
+      </div>
+      <ContentName>
+        <FaUserCircle color="#FF6109" size="20" />
+        <h2>Ciole Guilherme</h2>
+      </ContentName>
+      <ContentEmail>
+      <MdEmail color="#FF6109" size="20" />
+        <p>
+           guilhermeciope@email.com
+        </p>
+      </ContentEmail>
     </Container>
   );
 };
