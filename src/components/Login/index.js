7af7cc02873d0bb8f55c30246f869c8ae046b786
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-08-12 19:54:40 -0300

    feat: propriedade fullWidth nos inputs.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,8 +8,8 @@ const LoginForm = () => {
     return (
         <Container>
             <h1 className="title">Login</h1>
-            <TextField variant="outlined" label="E-mail" />
-            <TextField variant="outlined" label="Senha" />
+            <TextField fullWidth variant="outlined" label="E-mail" />
+            <TextField fullWidth variant="outlined" label="Senha" />
             <div className="button__container">
                 <button className="log__button">Entrar</button>
                 <span>Não tem cadastro? <Link>Inscreva-se!</Link></span>
