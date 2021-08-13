8798bb261e034032b0d4592087e7c5adf81e3253
Leandro <legato_soares@hotmail.com>
2021-08-13 10:41:43 -0300

    feat: adicionando container dos habitos

---
+++ src/components/HabitsContainer/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+overflow-y: scroll;
+display: flex;
+flex-flow: column nowrap;
+background-color: #000000 ;
+opacity: 91%;
+align-items: center;
+`
+export const Title = styled.span`
+font-size: 25px;
+font-weight: bolder;
+color: #FF6109;
+padding: 3rem;
+`
