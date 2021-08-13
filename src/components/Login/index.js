9e548031baa08029c9821d5ee6c1e4995a1df620
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-08-13 11:02:12 -0300

    fix: usuário no input de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,7 +8,7 @@ const LoginForm = () => {
     return (
         <Container>
             <h1 className="title">Login</h1>
-            <TextField fullWidth variant="outlined" label="E-mail" />
+            <TextField fullWidth variant="outlined" label="Usuário" />
             <TextField fullWidth variant="outlined" label="Senha" />
             <div className="button__container">
                 <button className="log__button">Entrar</button>
