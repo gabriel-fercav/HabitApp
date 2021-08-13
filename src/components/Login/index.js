f0d00cf9e7a15e3b30c48788f1272ab2b217d6c8
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-08-13 11:13:09 -0300

    feat: Provider do usuario concluido, consumindo todos os endpoints da api e armazenando no localStorage o token

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,24 +1,21 @@
-import { Container } from './styles'
-import { Link } from 'react-router-dom'
-import { TextField } from '@material-ui/core'
-
+import { Container } from "./styles";
+import { Link } from "react-router-dom";
+import { TextField } from "@material-ui/core";
 
 const LoginForm = () => {
+  return (
+    <Container>
+      <h1 className="title">Login</h1>
+      <TextField fullWidth variant="outlined" label="E-mail" />
+      <TextField fullWidth variant="outlined" label="Senha" />
+      <div className="button__container">
+        <button className="log__button">Entrar</button>
+        <span>
+          Não tem cadastro? <Link>Inscreva-se!</Link>
+        </span>
+      </div>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <h1 className="title">Login</h1>
-            <TextField fullWidth variant="outlined" label="E-mail" />
-            <TextField fullWidth variant="outlined" label="Senha" />
-            <div className="button__container">
-                <button className="log__button">Entrar</button>
-                <span>Não tem cadastro? <Link>Inscreva-se!</Link></span>
-            </div>
-
-        </Container>
-    )
-
-
-}
-
-export default LoginForm
+export default LoginForm;
