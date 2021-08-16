83b8a54ec66275ab3938111e617176fa05a24418
Leandro <legato_soares@hotmail.com>
2021-08-16 12:02:17 -0300

    feat: adicionando provider dos grupos e deixando funcional na aplicacao

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,11 +1,14 @@
 import { UserProvider } from "./UserProvider";
 import { HabitsProvider } from "./HabitsProvider";
+import { GroupsProvider } from "./GroupsProvider";
 
 const Providers = ({ children }) => {
   return (
   <UserProvider>
     <HabitsProvider>
-      {children}
+      <GroupsProvider>
+        {children}
+      </GroupsProvider>
     </HabitsProvider>
   </UserProvider>
   )
